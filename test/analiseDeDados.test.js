32604d0505b70f043f14b65c591478a59e5bbd6c
William Espindola Cardoso <williamcardoso_sc@hotmail.com>
2025-09-01 23:07:47 -0300

    novos testes Analise de Dados

--- test/analiseDeDados.test.js
+++ test/analiseDeDados.test.js
@@ -26,6 +26,35 @@ describe('AnaliseDeDados', () => {
   
     test('Deve calcular o desvio padrão corretamente', () => {
       expect(analise.calcularDesvioPadrao()).toBeCloseTo(14.14, 2);
-    });     
+    });
+    
+
+    // meus testes
+    test("adicionarDados deve adicionar elementos ao array", () => {
+      analise.adicionarDados([60, 70]);
+      expect(analise.dados).toEqual([10, 20, 30, 40, 50, 60, 70]);
+    });
+
+    test("calcularMedia deve retornar a média dos números", () => {
+      expect(analise.calcularMedia()).toBe(30);
+    });
+
+    test("calcularMediana deve funcionar para listas pares e ímpares", () => {
+      const analiseImpar = new AnaliseDeDados([1, 3, 2]);
+      expect(analiseImpar.calcularMediana()).toBe(2);
+
+      const analisePar = new AnaliseDeDados([1, 2, 3, 4]);
+      expect(analisePar.calcularMediana()).toBe(2.5);
+    });
+
+    test("normalizarDados deve normalizar para intervalo 0 a 1", () => {
+      expect(analise.normalizarDados()).toEqual([0, 0.25, 0.5, 0.75, 1]);
+    });
+
+    test("calcularCorrelacao deve retornar 1 para listas idênticas", () => {
+      expect(analise.calcularCorrelacao([10, 20, 30, 40, 50])).toBe(1);
+    });
+
+
   });
   
